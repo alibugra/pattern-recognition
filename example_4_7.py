e2c8028aa2d3c43112f55d4ccfafe1f141b77f06
alibugra <alibugra@alibugra.com>
2016-10-17 22:27:43 +0300

    Contour plot of ellipses from means mu1 and mu2

--- example_4_7.py
+++ example_4_7.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from matplotlib import pyplot as plt
+from scipy.stats import multivariate_normal
 
 # Sample scatter plots
 plt.scatter([1.1, 0.3], [0.3, 1.9])
@@ -20,4 +21,16 @@ plt.show()
 x, y = np.random.multivariate_normal(mean2, cov, 1000).T
 plt.plot(x, y, '.')
 plt.axis('equal')
+plt.show()
+
+# Contour plot of ellipses from means mu1 and mu2
+x, y = np.mgrid[-1:1:.01, -1:1:.01]
+pos = np.empty(x.shape + (2,))
+pos[:, :, 0] = x; pos[:, :, 1] = y
+rv = multivariate_normal(mean1, cov)
+plt.contourf(x, y, rv.pdf(pos))
+plt.show()
+
+rv = multivariate_normal(mean2, cov)
+plt.contourf(x, y, rv.pdf(pos))
 plt.show()
