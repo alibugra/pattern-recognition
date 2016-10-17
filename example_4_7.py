92298749854f3de596541cec6e96804ad693439f
alibugra <alibugra@alibugra.com>
2016-10-17 12:55:23 +0300

    1000 samples added to plot

--- example_4_7.py
+++ example_4_7.py
@@ -12,12 +12,12 @@ mean2 = [3, 3]
 cov = [[1.1, 0.3], [0.3, 1.9]]
 
 # Gaussian distributions for means mu1 and mu2
-x, y = np.random.multivariate_normal(mean1, cov).T
+x, y = np.random.multivariate_normal(mean1, cov, 1000).T
 plt.plot(x, y, '.')
 plt.axis('equal')
 plt.show()
 
-x, y = np.random.multivariate_normal(mean2, cov).T
+x, y = np.random.multivariate_normal(mean2, cov, 1000).T
 plt.plot(x, y, '.')
 plt.axis('equal')
 plt.show()
